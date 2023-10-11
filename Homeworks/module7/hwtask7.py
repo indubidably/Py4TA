ffb050019fe1df30a0f17c39993508cc7f386d99
Igor Smirnov <igor.smirnov@clarivate.com>
2023-10-11 12:57:32 +0400

    fixed in accordance with the comment

--- Homeworks/module7/hwtask7.py
+++ Homeworks/module7/hwtask7.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
+
 
 engine = create_engine("sqlite:///films_db.db")
 Base = declarative_base()
@@ -17,26 +17,4 @@ class Film(Base):
 
 Base.metadata.create_all(engine)
 
-Session = sessionmaker(bind=engine)
-session = Session()
 
-film1 = Film(title="Mars of Destruction", director="Johnson", release_year=2020)
-film2 = Film(title="The Madagascar Escape", director="Schrodinger", release_year=2018)
-film3 = Film(title="Oktoberfest", director="Mikael Gustafson", release_year=2022)
-
-session.add_all([film1, film2, film3])
-session.commit()
-
-film_to_update = session.query(Film).filter_by(title="Mars of Destruction").first()
-if film_to_update:
-    film_to_update.release_year = 2021
-    session.commit()
-
-films = session.query(Film).all()
-for film in films:
-    print(f"Title: {film.title}, Director: {film.director}, Release Year: {film.release_year}")
-
-session.query(Film).delete()
-session.commit()
-
-session.close()
