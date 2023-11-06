687b9cf409f85115f268f5f6d038151270b6a133
Igor Smirnov <igor.smirnov@clarivate.com>
2023-11-07 00:37:22 +0400

    module15 done

---
+++ Homeworks/module15.py
@@ -0,0 +1,16 @@
+import requests
+
+url = "https://api.punkapi.com/v2/beers/8"
+
+response_get = requests.get(url)
+body_get = response_get.json()
+assert response_get.status_code == 200
+assert body_get[0]["name"] == "Fake Lager"
+assert body_get[0]["abv"] == 4.7
+
+response_del = requests.delete(url)
+body_del = response_del.json()
+assert response_del.status_code == 404
+assert body_del["message"] == "No endpoint found that matches '/v2/beers/8'"
+
+
